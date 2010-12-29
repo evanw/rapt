5c437f20fd268de33c1eb098b4782deb3fd4ae69
Evan Wallace <evan@evan-wallaces-macbook-pro.local>
2010-12-29 16:19:34 -0500

    added doors, cogs, buttons, and signs to level converter

--- level_converter/src/main.cpp
+++ level_converter/src/main.cpp
@@ -1,40 +1,45 @@
 #include <iostream>
 #include <fstream>
 #include <stack>
+#include <list>
 #include "world.pb.h"
 using namespace std;
 
-// partial template specialization for strings
+// partial template specialization for strings and bools
 template <typename T> void val(ostream *o, T value) { *o << value; }
 template <> void val<const char *>(ostream *o, const char *value) { *o << '"' << value << '"'; }
+template <> void val<string>(ostream *o, string value) { *o << '"' << value << '"'; }
+template <> void val<bool>(ostream *o, bool value) { *o << (value ? "true" : "false"); }
 
 class JSON
 {
 private:
     enum { ARRAY, OBJECT };
 
+    bool pack;
     stack<int> s;
     ostream *o;
     bool first;
     int i;
 
-    string indent() { return string(2 * i, ' '); }
+    string indent() { return pack ? "" : string(2 * i, ' '); }
     bool in_array() { return !s.empty() && s.top() == ARRAY; }
 
     void newline_indent()
     {
-        *o << (first ? "\n" : ",\n") << indent();
+        if (pack) *o << (first ? "" : ",") << indent();
+        else *o << (first ? "\n" : ",\n") << indent();
         first = false;
     }
 
 public:
-    JSON(ostream *out) : o(out), first(true), i(1) { *o << "{"; }
-    ~JSON() { *o << "\n}\n"; }
+    JSON(ostream *out, bool should_pack) : pack(should_pack), o(out), first(true), i(1) { *o << "{"; }
+    ~JSON() { *o << (pack ? "}" : "\n}\n"); }
 
     JSON &name(const string &name)
     {
         newline_indent();
-        *o << '"' << name << "\": ";
+        *o << '"' << name << "\":" << (pack ? "" : " ");
         first = false;
         return *this;
     }
@@ -51,7 +56,7 @@ public:
         if (in_array()) newline_indent();
         *o << '[';
         val(o, x);
-        *o << ", ";
+        *o << "," << (pack ? "" : " ");
         val(o, y);
         *o << ']';
         return *this;
@@ -82,13 +87,13 @@ public:
         int t = s.top();
         first = false;
         i--;
-        *o << '\n' << indent() << (t == ARRAY ? ']' : '}');
+        *o << (pack ? "" : "\n") << indent() << (t == ARRAY ? ']' : '}');
         s.pop();
         return *this;
     }
 };
 
-void convert(const string &input, const string &output)
+void convert(const string &input, const string &output, bool pack)
 {
     // open input
     ifstream in(input.c_str(), ios::in | ios::binary);
@@ -114,21 +119,58 @@ void convert(const string &input, const string &output)
         return;
     }
 
-    JSON json(&out);
+    JSON json(&out, pack);
     json.name("unique_id").value(world.unique_id());
-    json.name("width").value(world.width());
-    json.name("height").value(world.height());
+    json.name("width").value(world.width() * 8);
+    json.name("height").value(world.height() * 8);
     json.name("start").value(world.players_start_x(), world.players_start_y());
     json.name("end").value(world.players_end_x(), world.players_end_y());
     json.name("entities").array();
 
-    for (int d = 0; d < world.door_size(); d++)
+    for (int i = 0; i < world.door_size(); i++)
     {
-        const FileDoor &door = world.door(d);
+        const FileDoor &door = world.door(i);
         json.object();
         json.name("class").value("wall");
         json.name("start").value(door.start_x(), door.start_y());
         json.name("end").value(door.end_x(), door.end_y());
+        json.name("oneway").value(door.type() == FileDoor::ONE_WAY);
+        json.name("open").value(door.state() == FileDoor::DOOR_OPEN);
+        json.name("color").value(door.color());
+        json.end();
+    }
+
+    for (int i = 0; i < world.cog_size(); i++)
+    {
+        const FileCog &cog = world.cog(i);
+        json.object();
+        json.name("class").value("cog");
+        json.name("pos").value(cog.cog_x(), cog.cog_y());
+        json.end();
+    }
+
+    for (int i = 0; i < world.button_size(); i++)
+    {
+        const FileButton &button = world.button(i);
+        json.object();
+        json.name("class").value("button");
+        json.name("type").value(button.behavior());
+        json.name("pos").value(button.position_x(), button.position_y());
+        json.name("walls").array();
+        for (int j = 0; j < button.door_index_size(); j++)
+            json.value(button.door_index(j));
+        json.end();
+        json.name("color").value(button.color());
+        json.end();
+    }
+
+    for (int i = 0; i < world.sign_size(); i++)
+    {
+        const FileSign &sign = world.sign(i);
+        json.object();
+        json.name("class").value("sign");
+        json.name("pos").value(sign.sign_x(), sign.sign_y());
+        json.name("text").value(sign.text());
         json.end();
     }
 
@@ -137,20 +179,38 @@ void convert(const string &input, const string &output)
 
 int main(int argc, char *argv[])
 {
-    if (argc < 2)
+    list<string> args;
+    list<string>::iterator i;
+    for (int i = 1; i < argc; i++)
+        args.push_back(argv[i]);
+
+    // parse flags
+    bool pack = false;
+    for (i = args.begin(); i != args.end(); i++)
     {
-        cout << "usage: level_converter <lvl files>" << endl;
+        if (*i == "--pack")
+        {
+            pack = true;
+            args.erase(i);
+            break;
+        }
+    }
+
+    // print usage
+    if (args.empty())
+    {
+        cout << "usage: level_converter [--pack] <lvl files>" << endl;
         return 0;
     }
 
-    for (int i = 1; i < argc; i++)
+    // compile files
+    for (i = args.begin(); i != args.end(); i++)
     {
-        string input = argv[i];
+        string input = *i;
         string output = input;
         if (output.find(".lvl") == output.size() - 4) output = output.substr(0, output.size() - 4);
         output += ".json";
-
-        convert(input, output);
+        convert(input, output, pack);
     }
 
     return 0;
