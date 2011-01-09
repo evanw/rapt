b877ced47bc2ff9006b88ead97f5d11f27277529
Evan Wallace <evan@evan-wallaces-macbook-pro.local>
2011-01-08 23:28:11 -0500

    added index page: rails/app/views/application/index.html.erb

--- rails/config/routes.rb
+++ rails/config/routes.rb
@@ -63,7 +63,7 @@ Rapt::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => "users#show"
+  root :to => "application#index"
 
   # See how all your routes lay out with "rake routes"
 
