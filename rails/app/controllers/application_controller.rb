4a8963854aa865a84d6028129b0cb092587263e7
Evan Wallace <evan@evan-wallaces-macbook-pro.local>
2011-02-23 21:50:15 -0500

    added redirect to raptjs.com

--- rails/app/controllers/application_controller.rb
+++ rails/app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_filter :check_uri
+
+  def check_uri
+    redirect_to request.protocol + 'raptjs.com' + request.request_uri if request.host =~ /^raptjs.heroku.com/
+  end
+
   protect_from_forgery
 
   def index
