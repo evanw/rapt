2f7b0e30b6223e2978e18b796830884a62d5c090
Kayle Gishen <kaylegishen@gmail.com>
2011-03-28 10:10:14 -0400

    Added validation of user and level to Statistic

--- rails/app/models/statistic.rb
+++ rails/app/models/statistic.rb
@@ -1,6 +1,9 @@
 class Statistic < ActiveRecord::Base
   belongs_to :level
   belongs_to :user
+  
+  validates_presence_of :user
+  validates_presence_of :level
 
   attr_accessible :complete, :got_all_cogs
 end
